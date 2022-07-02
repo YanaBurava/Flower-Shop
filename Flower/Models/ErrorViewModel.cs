c8717348d661d0c8ca0ddfb012adb7e3bc75381d
Янина Бурова <yaninaburova@gmail.com>
2022-07-02 08:07:55 +0300

    Добавьте файлы проекта.

---
+++ Flower/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Flower.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
